2896910e575a97c54ffa694b31334dbe282416c9
Kangyun Wang <wky@kangyuns-mbp.home>
2018-05-01 17:06:19 +0200

    Remove unused route.

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,26 +7,37 @@ const roomController = require('../controllers/roomController');
 const router = express.Router();
 
 // For now redirect the user
-router.get('/', musicController.featuredPlaylist);
+router.get('/', musicController.newReleased);
 
 router.get('/featured', musicController.featuredPlaylist);
 router.get('/releases', musicController.newReleased);
+
 router.get('/play', musicController.play);
 // router.get('/playlist', musicController.publicPlaylist);
 router.get('/playlist', musicController.publicPlaylist);
-router.post('/playlist/add/:uri', musicController.addToPlaylist);
+// router.post('/playlist/add/:uri', isLoggedIn, musicController.addToPlaylist);
 // router.get('/discover', musicController.featuredPlaylist);
 
 router.get('/album/:id', musicController.albumDetail);
 
 // Rooms
 router.get('/rooms', roomController.showRooms);
-router.get('/room/:slug', roomController.singleRoom);
-router.get('/rooms/new-room', roomController.roomForm);
-router.post('/rooms/new-room', roomController.addRoom);
+router.get('/room/:slug', isLoggedIn, roomController.singleRoom);
+router.get('/rooms/new-room', isLoggedIn, roomController.roomForm);
+router.post('/rooms/new-room', isLoggedIn, roomController.addRoom);
 
 // User controller
 router.get('/login', accountController.login);
 router.get('/auth', accountController.authorize);
+
+function isLoggedIn(req, res, next) {
+	console.log('Check logged in');
+
+	if (req.session.userId) {
+		return next();
+	}
+
+	res.redirect('/login');
+}
 
 module.exports = router;
